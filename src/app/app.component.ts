7c243c9c39a2d625b1ec1de655dfa579c49f39ec
intel68 <ajayiolawale732@gmail.com>
2024-02-16 16:20:49 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'dynamic_forms';
+  showForm = true;
+
+  constructor(private fb: FormBuilder) {}
+
+  form: FormGroup = this.fb.group({
+    name: [''],
+    email: [''],
+    city: [''],
+    contacts: this.fb.array([
+      this.fb.group({
+        number: [''],
+        type: [''],
+        description: [''],
+      }),
+    ]),
+  });
+
+  
+  get contacts() {
+    return this.form.get('contacts') as FormArray;
+  }
+  addContact(){
+    this.contacts.push(
+      this.fb.group({
+        number: [''],
+      type: [''],
+      description: [''],
+      })
+    );
+}
+
+deleteContact(i: number){
+  this.contacts.removeAt(i);
+  }
+
+  onSubmit(){
+    alert(JSON.stringify(this.form.value, null, 2));
+  }
 }
